e5aa5d5a30b57307f3505cda21a97a8c77807601
Ваня Мартинович <martsinovichivan@gmail.com>
2024-04-14 23:30:22 +0300

    encrypt and decrypt

--- Cipher/Cipher.cpp
+++ Cipher/Cipher.cpp
@@ -1,36 +1,112 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int randd()
+{
+    std::srand(std::time(nullptr));
+    int random = std::rand() % 256 - 128; // Генерируем случайное число в диапазоне от -128 до 127
+    return random;
+}
+
+
+void encrypt(vector<char>& v)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        v[i] = randd();
+    }
+
+
+}
+
+void decrypt(vector<char>& v, string name)
+{
+    char* rassh = new char [4];
+    for (int i = name.length() - 4; i < name.length(); i++)
+    {
+        rassh[i - name.length() - 4] = name[i];
+        cout << rassh[i - name.length() - 4];
+    }
+    if (rassh == ".png")
+    {
+        cout << "png";
+    }
+    else if (rassh == ".jpg")
+    {
+        cout << "jpg";
+    }
+    else if (rassh == "jpeg")
+    {
+        cout << "jpeg";
+    }
+    else
+    {
+        cout << ":(";
+    }
+    delete[] rassh;
+}
+
 
 int main() {
-    setlocale(LC_ALL, "russian");
-    const std::string imageFilePath = "D:\\logo.png"; // Укажите путь к вашему изображению
+    //setlocale(LC_ALL, "russian");
+    string imageFilePath = "D:\\Download\\1234.jpeg"; //"D:\\logo.png"; // Путь к изображению
+    // std::cin >> imageFilePath;
 
     // Открываем файл в бинарном режиме
-    std::ifstream imageFile(imageFilePath, std::ios::binary);
-    if (!imageFile.is_open()) {
-        std::cerr << "Ошибка при открытии файла: " << imageFilePath << std::endl;
+    std::ifstream imageFile(imageFilePath, ios::binary);
+    if (!imageFile.is_open()) 
+    {
+        cerr << "Ошибка при открытии файла: " << imageFilePath << "\n";
         return 1;
     }
 
     // Читаем данные из файла в буфер
-    std::vector<char> ImgBin;
+    vector<char> Img;
     char ch;
-    while (imageFile.get(ch)) {
-        ImgBin.push_back(ch);
+    while (imageFile.get(ch)) 
+    {
+        Img.push_back(ch);
     }
 
+
+    // Img[Img.size() - 1]
+
+    decrypt(Img, imageFilePath);
+    /*
     // Выводим бинарные данные изображения в консоль
-    for (const char& ch : ImgBin) {
-        std::cout << static_cast<int>(ch) << " "; // Выводим байты как целые числа
+    for (const char& ch : Img) 
+    {
+        cout << static_cast<int>(ch) << " "; // Выводим байты как целые числа
     }
-    std::cout << std::endl;
+    cout << "\n";
 
-    // Теперь у вас есть бинарные данные изображения в векторе 'buffer'
-    // Можете обрабатывать их по своему усмотрению
+    // Теперь у нас есть бинарные данные изображения в векторе 'buffer'
 
-    // Пример вывода размера изображения
-    std::cout << "Размер изображения: " << ImgBin.size() << " байт" << std::endl;
+    cout << "Размер изображения: " << Img.size() << " байт" << "\n";
+    */
+    /*
+    for (int i = 0; i < 8; i++)
+    {
+        cout << Img[i] << " ";
+    }
+    cout << "\n";
+    encrypt(Img);
+    for (int i = 0; i < 8; i++)
+    {
+        cout << Img[i] << " ";
+    }
+    cout << "\n";
+    Img[0] = -1; Img[1] = -40; Img[2] = -1; Img[3] = -32; Img[4] = 0; Img[5] = 16; Img[6] = 74; Img[7] = 70;
+    for (int i = 0; i < 8; i++)
+    {
+        cout << Img[i] << " ";
+    }
+    cout << "\n";
+    */
 
     // Закрываем файл
     imageFile.close();
